d1abf3e55b25d1f167fa17a24c94354c29d19e28
Yann Guineau <yann@chainsafe.io>
2021-07-28 20:16:00 -0400

    Fix eth signature

--- internal/eth/sign_verify.go
+++ internal/eth/sign_verify.go
@@ -12,11 +12,12 @@ func VerifySig(from, sigHex string, msg []byte) bool {
 	fromAddr := common.HexToAddress(from)
 
 	sig := hexutil.MustDecode(sigHex)
-	// https://github.com/ethereum/go-ethereum/blob/55599ee95d4151a2502465e0afc7c47bd1acba77/internal/ethapi/api.go#L442
-	if sig[64] != 27 && sig[64] != 28 {
-		return false
-	}
-	sig[64] -= 27
+
+	//// https://github.com/ethereum/go-ethereum/blob/55599ee95d4151a2502465e0afc7c47bd1acba77/internal/ethapi/api.go#L442
+	//if sig[64] != 27 && sig[64] != 28 {
+	//	return false
+	//}
+	//sig[64] -= 27
 
 	pubKey, err := crypto.SigToPub(SignHash(msg), sig)
 	if err != nil {
